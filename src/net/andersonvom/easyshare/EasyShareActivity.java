c6a27fbdc7d7a5d22cebfd54f1325b7035644c25
Anderson Mesquita <andersonvom@gmail.com>
2011-10-16 16:25:31 -0400

    Added basic layout and sample strings

--- src/net/andersonvom/easyshare/EasyShareActivity.java
+++ src/net/andersonvom/easyshare/EasyShareActivity.java
@@ -2,6 +2,8 @@ package net.andersonvom.easyshare;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class EasyShareActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +11,18 @@ public class EasyShareActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        ListView service_list = (ListView) findViewById(R.id.service_list);
+        service_list.setAdapter(
+        		new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, SERVICES)
+        );
     }
+    
+    static final String[] SERVICES = new String[] {
+    	"Facebook",
+    	"Twitter",
+    	"Imgur",
+    	"Picasa",
+    	"TwitPic"
+    };
 }
