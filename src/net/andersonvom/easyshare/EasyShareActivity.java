9b270435285330da780771d865bbf583da172b7b
Anderson Mesquita <andersonvom@gmail.com>
2011-10-16 18:09:29 -0400

    Added basic functionality of adding test data to database and list it

--- src/net/andersonvom/easyshare/EasyShareActivity.java
+++ src/net/andersonvom/easyshare/EasyShareActivity.java
@@ -1,28 +1,69 @@
 package net.andersonvom.easyshare;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.database.Cursor;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SimpleCursorAdapter;
 
-public class EasyShareActivity extends Activity {
+public class EasyShareActivity extends ListActivity
+{
+	
+	private ServiceDbAdapter dbHelper;
+	public static final int MENU_ADD_ID = Menu.FIRST;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
-        ListView service_list = (ListView) findViewById(R.id.service_list);
-        service_list.setAdapter(
-        		new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, SERVICES)
-        );
+        dbHelper = new ServiceDbAdapter(this);
+        dbHelper.open();
+        fillServiceList();
     }
     
-    static final String[] SERVICES = new String[] {
-    	"Facebook",
-    	"Twitter",
-    	"Imgur",
-    	"Picasa",
-    	"TwitPic"
-    };
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+    	boolean result = super.onCreateOptionsMenu(menu);
+    	menu.add(0, MENU_ADD_ID, 0, R.string.add_service);
+    	
+    	return result;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+    	switch (item.getItemId())
+    	{
+    		case MENU_ADD_ID:
+    			createService();
+    			return true;
+    	}
+    	
+    	return super.onOptionsItemSelected(item);
+    }
+    
+    private void createService()
+    {
+    	Service service = new Service();
+    	service.setName("Teste");
+    	service.setEmail("test@example.com");
+    	dbHelper.create(service);
+    	fillServiceList();
+    }
+    
+    private void fillServiceList()
+    {
+    	Cursor cursor = dbHelper.fetch();
+    	startManagingCursor(cursor);
+    	
+    	String[] from = new String[] { ServiceDbAdapter.KEY_NAME };
+    	int[] to = new int[] { android.R.id.text1 };
+    	
+    	SimpleCursorAdapter services =
+    		new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, cursor, from, to);
+    	setListAdapter(services);
+    }
 }
